83a84ea30572b9682df6413e2aa69d295ec6c260
Anneliese Braunegg <akb89@case.edu>
2021-02-20 19:03:54 -0500

    Added method docstrings; added payments array; created main method; moved code from command line interaction section to main method

--- python_exploration.py
+++ python_exploration.py
@@ -14,31 +14,59 @@ Assignment 2: Part 2"""
 # TODO: Fill in customers array
 customers = []
 
+# TODO: Fill in payments array
+payments = []
+
 
 def print_out_payment_history_header():
+    """Print out the header for a customer payment history report."""
     # TODO
     pass
 
 
 def print_out_customer_payment_history(cust_index):
+    """
+    Print out the payment history for the customer at the specified index in the
+    customers array.
+
+    :param cust_index: The index of the customer
+    """
     # TODO
     pass
 
 
 def account_standing(acct_index):
+    """
+    Return the account standing for the account at the specified index in the
+    payments array.
+
+    :param acct_index: The index of the account in the payments array
+
+    :return: The standing of the account
+    """
     # TODO
     pass
 
 
 def num_of_zero_payments(acct_index):
+    """
+    Return the number of zero (0) payments that exist for the account at the
+    specified index in the payments array.
+
+    :param acct_index: The index of the account in the payments array
+
+    :return: The number of zero (0) payments that exist for the account
+    """
     # TODO
     pass
 
-
-if __name__ == "__main__":
+def main():
     # Print out report header
     print_out_payment_history_header()
 
     # Print the payment history for each customer in the customers array.
     for cust_index in range(0, len(customers)):
         print_out_customer_payment_history(cust_index)
+
+if __name__ == "__main__":
+    main()
